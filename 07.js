702d3e9e72f04b3108d705d8a27d0f9c90730723
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2025-04-06 20:16:37 +0530

    Revise

---
+++ 07.js
@@ -0,0 +1,17 @@
+// Onjects -> ALso called object literal
+
+/* 
+Object store values in key-value pair. 
+It is reference data type.
+Object dont have index
+*/
+
+const person = {
+  name: "LaxmiNarayana",
+  age: 23,
+  profession: "Software Engineer",
+  country: "India",
+};
+
+console.log(person);
+console.log(person.country);
